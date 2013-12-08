67b178fc5e899b339b53680cdc970ec7aa1899c5
Georg Berendt <georg.berendt@outlook.com>
2013-12-08 21:12:48 +0100

    Added a test for class path change

--- slr/src/test/java/com/xafero/slr/util/HelperTest.java
+++ slr/src/test/java/com/xafero/slr/util/HelperTest.java
@@ -1,6 +1,9 @@
 package com.xafero.slr.util;
 
-// import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.junit.Test;
 
@@ -19,5 +22,14 @@ public class HelperTest {
 	@Test(expected = RuntimeException.class)
 	public void testInvoke() {
 		RuntimeHelper.invoke(null, null);
+	}
+
+	@Test
+	public void testClasspath() {
+		URLClassLoader loader = (URLClassLoader) ClassLoader
+				.getSystemClassLoader();
+		URL url = loader.getURLs()[0];
+		int result = RuntimeHelper.extendClassPath(url);
+		assertEquals(0, result);
 	}
 }
