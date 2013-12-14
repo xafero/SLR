6372743b0d0714a95fb6211b1fbfdfe339d14d49
Georg Berendt <georg.berendt@outlook.com>
2013-12-14 19:26:04 +0100

    Added a file executing option

--- slr/src/main/java/com/xafero/slr/App.java
+++ slr/src/main/java/com/xafero/slr/App.java
@@ -1,6 +1,8 @@
 package com.xafero.slr;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -15,6 +17,7 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 
 import com.xafero.slr.api.IRuntime;
+import com.xafero.slr.util.IOHelper;
 
 public class App {
 	private static final String DEFAULT_CFG_FILE = "scriptEngines.cfg.xml";
@@ -39,6 +42,9 @@ public class App {
 		options.addOption(OptionBuilder.withLongOpt("exec")
 				.withDescription("execute one line of code").hasArg()
 				.withArgName("line").create("e"));
+		options.addOption(OptionBuilder.withLongOpt("run")
+				.withDescription("run one script file").hasArg()
+				.withArgName("file").create("f"));
 		// Parse the textual input
 		CommandLineParser parser = new BasicParser();
 		CommandLine cmd = parser.parse(options, args);
@@ -70,18 +76,7 @@ public class App {
 		ScriptEngine engine = null;
 		if (cmd.hasOption("l")) {
 			String lang = cmd.getOptionValue("l");
-			String artf = usrCfg.getProperty(lang);
-			if (artf == null)
-				artf = defCfg.getProperty(lang);
-			if (artf != null) {
-				IRuntime rt = Runtime.getInstance();
-				rt.require(artf);
-			}
-			ScriptEngineManager mgr = new ScriptEngineManager();
-			engine = mgr.getEngineByExtension(lang);
-			if (engine == null)
-				throw new UnsupportedOperationException(
-						"Couldn't find an engine for '" + lang + "'!");
+			engine = getEngine(lang, defCfg, usrCfg);
 		}
 		// Check if line's execution is wanted
 		if (cmd.hasOption("e")) {
@@ -90,8 +85,36 @@ public class App {
 			engine.eval(line);
 			return;
 		}
+		// Check if file's execution is wanted
+		if (cmd.hasOption("f")) {
+			String path = cmd.getOptionValue("f");
+			File file = new File(path);
+			if (engine == null) {
+				String lang = IOHelper.last(file.getName().split("\\."));
+				engine = getEngine(lang, defCfg, usrCfg);
+			}
+			engine.eval(new FileReader(file));
+			return;
+		}
 		// Just panic here
 		throw new UnsupportedOperationException("Can't handle '"
 				+ Arrays.toString(args) + "'!");
+	}
+
+	private ScriptEngine getEngine(String lang, Properties defCfg,
+			Properties usrCfg) {
+		String artf = usrCfg.getProperty(lang);
+		if (artf == null)
+			artf = defCfg.getProperty(lang);
+		if (artf != null) {
+			IRuntime rt = Runtime.getInstance();
+			rt.require(artf);
+		}
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByExtension(lang);
+		if (engine == null)
+			throw new UnsupportedOperationException(
+					"Couldn't find an engine for '" + lang + "'!");
+		return engine;
 	}
 }
